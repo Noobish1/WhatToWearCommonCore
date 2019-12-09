8e8f8b572e84e49c2674696db8e18e661ffbb9a0
Blair McArthur <blair.mcarthur@icloud.com>
2019-12-08 22:34:24 -0500

    Added the NonEmptyCaseIterable protocol + specs

---
+++ WhatToWearCommonCoreTests/Specs/Protocols/NonEmptyCaseIterableSpec.swift
@@ -0,0 +1,29 @@
+import Quick
+import Nimble
+import WhatToWearCommonCore
+
+private enum TestEnum: NonEmptyCaseIterable {
+    case first
+    case second
+    case third
+}
+
+internal final class NonEmptyCaseIterableSpec: QuickSpec {
+    internal override func spec() {
+        describe("NonEmptyCaseIterable") {
+            describe("its nonEmptyCases") {
+                var expected: NonEmptyArray<TestEnum>!
+                var actual: NonEmptyArray<TestEnum>!
+                
+                beforeEach {
+                    expected = NonEmptyArray<TestEnum>(elements: .first, .second, .third)
+                    actual = TestEnum.nonEmptyCases
+                }
+                
+                it("should return an array of all cases") {
+                    expect(actual) == expected
+                }
+            }
+        }
+    }
+}
